441516a179a31c90cdbc21e52ea43921128e87c7
Chris Bolin <bolin.chris@gmail.com>
2021-03-16 00:12:28 -0600

    beginning of props handling

--- victor/victor.pl
+++ victor/victor.pl
@@ -1,13 +1,18 @@
 use strict;
 use warnings;
 
+use Data::Dumper;
+
 main();
 
 sub main {
     my $path = "$0image.svg";
     $path =~ s/victor\.pl//;
     write_file($path, demo());
-    print 'Finished.';
+    
+    print `cat $path`;
+
+    print "\nFinished.";
 }
 
 sub write_file {
@@ -23,14 +28,42 @@ sub write_file {
 }
 
 sub demo {
-    return <<'EOF';
+    my $letters = 'EL FIN';
+    my $inside = <<"EOF";
+      <rect width="100%" height="100%" fill="red" />
+      <circle cx="150" cy="100" r="80" fill="green" />
+      <text x="150" y="120" font-size="60" text-anchor="middle" fill="white">
+        $letters
+      </text>
+EOF
+    return svg([$inside]);
+}
+
+sub get_props {
+    my ($thing) = @_;
+    
+    # if it's a hash, return it
+    return $thing if ref($thing) eq 'HASH';
+    return { children => $thing } if ref($thing) eq 'ARRAY';
+    return { children => [$thing] } if ref($thing) eq '';
+}
+
+sub props {
+    my $props = get_props(@_);
+    $props->{child} = join(" ", @{$props->{children}});
+    return $props;
+}
+
+sub svg {
+    my $props = props(@_);
+
+    return <<"EOF";
 <svg version="1.1"
    baseProfile="full"
    width="300" height="200"
    xmlns="http://www.w3.org/2000/svg">
-      <rect width="100%" height="100%" fill="red" />
-      <circle cx="150" cy="100" r="80" fill="green" />
-      <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+    $props->{child}
 </svg>
 EOF
 }
+
